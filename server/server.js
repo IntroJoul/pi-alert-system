ed6a2149e577cdd333a5971ca0fcec99f5b8aafe
IntroJoul <introjouldev@gmail.com>
2022-06-28 20:18:29 +0100

    not using routes because there is no point

--- server/server.js
+++ server/server.js
@@ -4,7 +4,11 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/activate"));
+
+app.get("/activate", (req, res) => {
+  res.send("it works");
+  console.log("it works");
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port: ${port}`);
