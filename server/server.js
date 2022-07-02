001702de5c9f5ba5d0c3f96403559f0ad0031a81
IntroJoul <introjouldev@gmail.com>
2022-07-02 14:38:33 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static("build"));
 
 let activated = false;
 
